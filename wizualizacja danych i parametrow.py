dd9a390fb22f19593b49af8f75f571e55107b409
MatFayden <mateusz.sakiewicz@gmail.com>
2026-04-07 14:26:46 +0200

    Dodanie funkcji obliczajacej R2 oraz wizualizacji danych

--- wizualizacja danych i parametrow.py
+++
@@ -1,5 +0,0 @@
-def rysuj_funkcje_kosztu(iteracje, koszty):
-    for i in range(len(iteracje)):
-        print(f"Iteracja: {iteracje[i]} - Wartość kosztu: {koszty[i]}")
-
-rysuj_funkcje_kosztu([1, 2, 3, 4], [100, 50, 25, 10])
